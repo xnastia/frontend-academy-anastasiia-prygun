fbc62b49e2e52bd671724288987f43bf18405e21
Anastasiia Prygun <anastasiia.prygun@gmail.com>
2019-03-20 16:18:24 +0200

    added modal-box and web api

---
+++ Fooseshoes.WebApi/Fooseshoes.WebSite/gulpfile.js
@@ -0,0 +1,13 @@
+﻿/// <binding Clean='clean' />
+"use strict";
+
+var gulp = require("gulp"),
+    rimraf = require("rimraf"),
+    sass = require("gulp-sass");
+
+
+gulp.task("sass", function () {
+    return gulp.src('sass/styles.scss')
+            .pipe(sass("/css"))
+            
+});
