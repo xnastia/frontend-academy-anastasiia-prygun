b32caadcc7f593bf25310ab745b5e45ff8d635be
Anastasiia Prygun <anastasiia.prygun@gmail.com>
2019-03-20 16:48:35 +0200

    added api

---
+++ Fooseshoes/Fooseshoes.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fooseshoes.WebApi.WebApiApplication" Language="C#" %>
---
+++ Fooseshoes/Fooseshoes.WebSite/scripts/switchLoginRegister.js
@@ -0,0 +1,27 @@
+﻿function switchLoginRegister(element) {
+    var registerBtnClasses = document.getElementById("register-btn").classList;
+    var loginBtnClasses = document.getElementById("login-btn").classList;
+    
+    if (element.id == "login-btn") {
+        registerBtnClasses.remove("modal-nav-btn-active");
+        registerBtnClasses.add("modal-nav-btn");
+        loginBtnClasses.remove("modal-nav-btn");
+        loginBtnClasses.add("modal-nav-btn-active");
+        document.getElementById("login-form").style.display = "block";
+        document.getElementById("register-form").style.display = "none";
+        document.getElementById("terms-form").style.display = "none";
+        document.getElementById("register-footer").style.display = "none";
+        document.getElementById("login-footer").style.display = "block";
+    }
+    else{
+        registerBtnClasses.remove("modal-nav-btn");
+        registerBtnClasses.add("modal-nav-btn-active");
+        loginBtnClasses.remove("modal-nav-btn-active");
+        loginBtnClasses.add("modal-nav-btn");
+        document.getElementById("login-form").style.display = "none";
+        document.getElementById("register-form").style.display = "block";
+        document.getElementById("terms-form").style.display = "block";
+        document.getElementById("login-footer").style.display = "none";
+        document.getElementById("register-footer").style.display = "block";
+        }
+    }    
